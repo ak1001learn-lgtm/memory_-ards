7e7cf03137e1ac03b97fc6da593716322f67da4c
ak1001learn-lgtm <312964786+ak1001learn-lgtm@users.noreply.github.com>
2026-08-19 12:37:24 +0300

    add mode radions and buttons to start screen

--- app/start_screen.py
+++ app/start_screen.py
@@ -1,7 +1,10 @@
 from PyQt6.QtGui import QFont
 from PyQt6.QtWidgets import  (
+    QButtonGroup,
+    QHBoxLayout,
     QLabel,
     QPushButton,
+    QRadioButton,
     QVBoxLayout, 
     QWidget
 )
@@ -27,5 +30,40 @@ class StartScreen(QWidget):
         layout.addSpacing(12)
 
         study_button = QPushButton("начать обучение")
-        study_button.setMinimumHeight(48)
+        study_button.setMinimumHeight(40)
         layout.addWidget(study_button)
+
+        modes_label = QLabel("режим обучения")
+        layout.addWidget(modes_label)
+
+        radio_new_only = QRadioButton("только новые")
+        radio_all      = QRadioButton("все карточки")
+        radio_practice = QRadioButton("новые с повторами")
+        radio_new_only.setChecked(True)
+
+        mode_radios = [
+            radio_new_only, 
+            radio_all,
+            radio_practice,
+        ]
+
+        mode_group = QButtonGroup(self)
+        moeds_layout = QHBoxLayout()
+
+        for radio in mode_radios:
+            mode_group.addButton(radio)
+            moeds_layout.addWidget(radio)
+
+        # moeds_layout.addStretch()
+        layout.addLayout(moeds_layout)
+
+        # QCheckBox для тегов
+        #QScrollArea 
+
+        settings_button = QPushButton("настойки")
+        quit_button    = QPushButton("выйти")
+
+        bottom_layout = QHBoxLayout()
+        bottom_layout.addWidget(settings_button)
+        bottom_layout.addWidget(quit_button)
+        layout.addLayout(bottom_layout)
